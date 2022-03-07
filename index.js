c8cdcebeffac43a7b845f4e6f040ae2e3c9be18b
Ariane Crestani <ariane.mrv@gmail.com>
2022-03-07 10:31:48 +0100

    add items

---
+++ index.js
@@ -0,0 +1,13 @@
+//select itens
+
+const img = document.getElementById('avatar-1')
+const author = document.getElementById('author')
+const job = document.getElementById('job')
+const info = document.getElementById('info')
+
+const prevBtn = document.querySelector ('.prev-btn')
+const prevBtn = document.querySelector ('.next-btn')
+const prevBtn = document.querySelector ('.random-btn')
+
+
+// set starting item
