09d2c4ca6eee1f234795004dcae0f903e3337ebe
Ariane Crestani <ariane.mrv@gmail.com>
2022-03-07 12:35:30 +0100

    style

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const reviews = [
     {
       id: 1,
@@ -39,14 +40,14 @@ const reviews = [
 
 // select itens
 
-const img = document.getElementById('avatar-1')
+const img = document.getElementById("person-img")
 const author = document.getElementById('author')
 const job = document.getElementById('job')
 const info = document.getElementById('info')
 
 const prevBtn = document.querySelector ('.prev-btn')
-const prevBtn = document.querySelector ('.next-btn')
-const prevBtn = document.querySelector ('.random-btn')
+const nextBtn = document.querySelector ('.next-btn')
+const randomBtn = document.querySelector ('.random-btn')
 
 
 // set starting item
@@ -56,11 +57,36 @@ let currentItem = 0;
 //load inictial item
 
 document.addEventListener('DOMContentLoaded', function () {
+   showPerson(currentItem)
+})
+
+// show person based on item 
+
+const showPerson = () => {
     const item = reviews[currentItem]
     img.src = item.img;
     author.textContent = item.name
     job.textContent = item.job
     info.textContent = item.text
+
+}
+
+//show next person
+
+nextBtn.addEventListener('click', function (){
+    currentItem ++
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson(currentItem)
 })
 
-// show person based on item 
+//show previus person
+
+prevBtn.addEventListener('click', function (){
+    currentItem --
+    if(currentItem < 0){
+        currentItem = reviews.length -1;
+    }
+    showPerson(currentItem)
+})
